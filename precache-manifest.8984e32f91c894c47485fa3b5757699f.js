458d7e8ae88036fe18622eab2c35cc7ae1cc3c3f
Arina Pavlova <avladpavlova@sberbank.ru>
2020-12-12 09:36:19 +0300

    Updates

---
+++ precache-manifest.8984e32f91c894c47485fa3b5757699f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e554cff9bc88c01eaa07ae7b55c64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99398723a420e13b5078",
+    "url": "/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "99398723a420e13b5078",
+    "url": "/static/js/2.0b888e0a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0b888e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d71ddc6acb771c2f790",
+    "url": "/static/js/main.61b80f31.chunk.js"
+  },
+  {
+    "revision": "fafdbaf2638d17c06ad4",
+    "url": "/static/js/runtime-main.8e75aea5.js"
+  }
+]);
